7946747a0048ff5edcf9f7470701a4ffe931a422
Abhinay Jangde <abhinayjangde@gmail.com>
2025-01-13 13:10:03 +0530

    theme blinking problem fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,24 +3,29 @@ import type { Metadata } from "next"
 import Navbar from "@/components/Navbar"
 import Footer from "@/components/Footer"
 import AuthProvider from "@/providers/AuthProvider"
-import ThemeProvider from "@/providers/ThemeProvider"
+import { ThemeProvider } from "@/providers/ThemeProvider"
 
 export const metadata: Metadata = {
   title: "The right way to learn coding",
   description: "The right way to learn coding",
 }
 
-const RootLayout = ({children}: Readonly<{ children: React.ReactNode }>) => {
+const RootLayout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <AuthProvider>
-        <ThemeProvider>
-          <body className="">
+        <body className="">
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
             <Navbar />
             {children}
             <Footer />
-          </body>
-        </ThemeProvider>
+          </ThemeProvider>
+        </body>
       </AuthProvider>
     </html>
   )
