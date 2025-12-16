bb3302737e5a8bf075513217acea4d1d7ea60d10
Abhinay Jangde <abhinayjangde@gmail.com>
2025-12-16 18:10:54 +0530

    fix product reditection issues

--- src/proxy.ts
+++ src/proxy.ts
@@ -10,7 +10,9 @@ export default function proxy(request: NextRequest) {
   }
 
   // Auth protection for dashboard
-  const sessionCookie = request.cookies.get("better-auth.session_token");
+  const sessionCookie =
+    request.cookies.get("__Secure-better-auth.session_token") ||
+    request.cookies.get("better-auth.session_token");
   if (!sessionCookie && request.nextUrl.pathname.startsWith("/dashboard")) {
     return NextResponse.redirect(new URL("/login", request.url));
   }
